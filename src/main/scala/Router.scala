d0bbbf36d7cf1ba9cb912d499d3f74ef24742388
Mirza Hodovic <mhodovic2@etf.unsa.ba>
2020-12-31 15:14:31 +0100

    Crossbar tryout

--- src/main/scala/Router.scala
+++ src/main/scala/Router.scala
@@ -1,8 +1,10 @@
 package router
+
 import chisel3.experimental._
 import chisel3._
 import chisel3.util.{Cat, Enum, is, switch}
 import org.scalacheck.Prop.False
+import xbar.XBar
 
 
 class TX() extends Bundle{
@@ -31,7 +33,6 @@ class Router extends MultiIOModule{
     val out_D = new TX()
     val out_L = new TX()
     val out_R = new TX()
-
   })
 
   val size = 32
@@ -43,20 +44,37 @@ class Router extends MultiIOModule{
   val out_L_dout = WireInit(0.U(size.W))
   val out_R_dout = WireInit(0.U(size.W))
 
-  io.out_U.dout := out_U_dout
-  io.out_D.dout := out_D_dout
-  io.out_L.dout := out_L_dout
-  io.out_R.dout := out_R_dout
-
   io.out_U.write := 0.U
   io.out_D.write := 0.U
   io.out_L.write := 0.U
   io.out_R.write := 0.U
 
+  //--------------Cross Bar--------------------
+  val XBar_U = Module(new XBar(size))
+  XBar_U.io.xbar_sele := "b0001".U
+  XBar_U.io.xbar_data_in := io.in_D.din | io.in_U.din | io.in_L.din | io.in_R.din
+  
+  val XBar_D = Module(new XBar(size))
+  XBar_D.io.xbar_sele := "b0010".U
+  XBar_D.io.xbar_data_in := io.in_D.din | io.in_U.din | io.in_L.din | io.in_R.din
+
+  val XBar_L = Module(new XBar(size))
+  XBar_L.io.xbar_sele := "b0100".U
+  XBar_L.io.xbar_data_in := io.in_D.din | io.in_U.din | io.in_L.din | io.in_R.din
+
+  val XBar_R = Module(new XBar(size))
+  XBar_R.io.xbar_sele := "b1000".U
+  XBar_R.io.xbar_data_in := io.in_D.din | io.in_U.din | io.in_L.din | io.in_R.din
+
+  io.out_U.dout := XBar_D.io.xbar_data_out_U | XBar_L.io.xbar_data_out_U | XBar_R.io.xbar_data_out_U
+  io.out_D.dout := XBar_U.io.xbar_data_out_D | XBar_L.io.xbar_data_out_D | XBar_R.io.xbar_data_out_D
+  io.out_L.dout := XBar_U.io.xbar_data_out_L | XBar_D.io.xbar_data_out_L | XBar_R.io.xbar_data_out_L
+  io.out_R.dout := XBar_U.io.xbar_data_out_R | XBar_D.io.xbar_data_out_R | XBar_L.io.xbar_data_out_R
+
   val empty :: full :: Nil = Enum(2)
   val stateReg = RegInit(empty)
 
-  val dataReg = WireInit(0.U(32))
+  //val dataReg = WireInit(0.U(32))
   val dataReg_U = RegInit(0.U(size.W))
   val dataReg_D = RegInit(0.U(size.W))
   val dataReg_L = RegInit(0.U(size.W))
--- src/main/scala/XBar.scala
+++ src/main/scala/XBar.scala
@@ -1,40 +1,42 @@
+package xbar
+
 import chisel3._
 
 class XBar(size : Int) extends Module{
   val io = IO(new Bundle{
     val xbar_sele = Input(UInt(4.W))
     val xbar_data_in = Input(UInt(size.W))
-    val xbar_data_out_N = Output(UInt(size.W))
-    val xbar_data_out_S = Output(UInt(size.W))
-    val xbar_data_out_E = Output(UInt(size.W))
-    val xbar_data_out_W = Output(UInt(size.W))
+    val xbar_data_out_U = Output(UInt(size.W))
+    val xbar_data_out_D = Output(UInt(size.W))
+    val xbar_data_out_R = Output(UInt(size.W))
+    val xbar_data_out_L = Output(UInt(size.W))
     //val xbar_data_out_L = Output(UInt(size.W))
 
   })
   val seleReg = WireInit(0.U(4.W))
   val dataReg = WireInit(0.U(size.W))
   //val xbar_data_out_L = WireInit(0.U(size.W))
-  val xbar_data_out_N = WireInit(0.U(size.W))
-  val xbar_data_out_E = WireInit(0.U(size.W))
-  val xbar_data_out_S = WireInit(0.U(size.W))
-  val xbar_data_out_W = WireInit(0.U(size.W))
-  io.xbar_data_out_E := xbar_data_out_E
-  io.xbar_data_out_S := xbar_data_out_S
-  io.xbar_data_out_W := xbar_data_out_W
-  io.xbar_data_out_N := xbar_data_out_N
+  val xbar_data_out_U = WireInit(0.U(size.W))
+  val xbar_data_out_R = WireInit(0.U(size.W))
+  val xbar_data_out_D = WireInit(0.U(size.W))
+  val xbar_data_out_L = WireInit(0.U(size.W))
+  io.xbar_data_out_R := xbar_data_out_R
+  io.xbar_data_out_D := xbar_data_out_D
+  io.xbar_data_out_L := xbar_data_out_L
+  io.xbar_data_out_U := xbar_data_out_U
   //io.xbar_data_out_L := xbar_data_out_L
 
   dataReg := io.xbar_data_in
   seleReg := io.xbar_sele
 
   when(seleReg === "b0001".U){
-    xbar_data_out_N :=  dataReg
+    xbar_data_out_U :=  dataReg
   }.elsewhen(seleReg === "b0010".U){
-    xbar_data_out_S :=  dataReg
+    xbar_data_out_D :=  dataReg
   }.elsewhen(seleReg === "b0100".U){
-    xbar_data_out_W :=  dataReg
+    xbar_data_out_L :=  dataReg
   }.elsewhen(seleReg === "b1000".U){
-    xbar_data_out_E :=  dataReg
+    xbar_data_out_R :=  dataReg
   }
 
 }
